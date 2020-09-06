9c3149724c79cc81f9b9e45b801edf53d2dfd723
Diviner <1437748401@qq.com>
2020-09-07 01:18:43 +0800

    Modified threeSum solution

--- Week_01/threeSum_1.java
+++ Week_01/threeSum_1.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class threeSum_1 {
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum_Method1(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
@@ -35,5 +35,30 @@ public class threeSum_1 {
             }
         }
         return ans;
+    }
+
+    public List<List<Integer>> threeSum_Method2(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int k = 0; k < nums.length - 2; k++) {
+            if (nums[k] > 0) {
+                break;
+            }
+            if(k > 0 && nums[k] == nums[k - 1]) continue;
+            int i = k + 1, j = nums.length - 1;
+            while (i < j) {
+                int sum = nums[k] + nums[i] + nums[j];
+                if (sum > 0) {
+                    while (i < j && nums[j] == nums[--j]);
+                } else if (sum < 0) {
+                    while (i < j && nums[i] == nums[++i]);
+                } else {
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k], nums[i], nums[j])));
+                    while(i < j && nums[i] == nums[++i]);
+                    while(i < j && nums[j] == nums[--j]);
+                }
+            }
+        }
+        return res;
     }
 }
